8eacd2f61baa7130bf594e20ea2fbf0d17b9befc
Jitendra Rathore <jitendra.rathore.iitm@gmail.com>
2025-11-17 14:58:22 +0000

    added Login and register stuffs

--- backend/application/main.py
+++ backend/application/main.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_bcrypt import Bcrypt
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
 
 class Base(DeclarativeBase):
   pass
@@ -10,17 +12,21 @@ db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///chat_db.sqlite3"
+app.config["JWT_SECRET_KEY"] = "super-secret-key"  
+jwt = JWTManager(app)
+api = Api(app)
+
 
 db.init_app(app)
 bcrypt = Bcrypt(app)
 from .models import User
 with app.app_context():
     db.create_all()
-    email = "Jitendra@rockedict.in"
+    email = "jitendra@rockedict.in"
     existing_user = db.session.execute(db.select(User).filter_by(email = email)).scalar_one_or_none()
     if not existing_user:
-        db.session.add(User(name = "Jitendra", email = email, password = bcrypt.generate_password_hash("Jitendra123")))
+        db.session.add(User(name = "Jitendra", email = email, password = bcrypt.generate_password_hash("Jitendra123").decode("utf-8"), role = "Admin"))
         db.session.commit()
 
-from .routes import *
+from . import routes
 
--- backend/application/routes.py
+++ backend/application/routes.py
@@ -1,5 +1,45 @@
-from .main import app
+from .main import api, db, bcrypt
+from .models import User
+from flask import request
+from flask_restful import Resource
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
-@app.route("/")
-def hello():
-    return "Hello"
+
+
+class Register(Resource):
+    def post(self):
+        name = request.json.get("name")
+        email = request.json.get("email")
+        password = request.json.get("password")
+        if not name or not email or not password:
+            return {"message": "All fields are required"}, 400
+        email = email.lower()
+        existing_user = db.session.execute(db.select(User).filter_by(email = email)).scalar_one_or_none()
+        if existing_user:
+            return {"message": f"{name}, you have already an account. please login"}, 400
+        db.session.add(User(name = name, email = email, password = bcrypt.generate_password_hash(password).decode("utf-8")))
+        db.session.commit()
+        return {"message": f"{name}, your account is created successfully"}, 201
+
+class Login(Resource):
+    def post(self):
+        email = request.json.get("email")
+        password = request.json.get("password")
+        if not email or not password:
+            return {"message": "All fields are required"}, 400
+        email = email.lower()
+        user = db.session.execute(db.select(User).filter_by(email = email)).scalar_one_or_none()
+        if user and bcrypt.check_password_hash(user.password, password) :
+            access_token = create_access_token(identity=user.email)
+            return {"access_token": access_token}, 200
+        return {"message": "Invalid credentials"}, 401
+
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"message": "Hello World"}
+
+api.add_resource(HelloWorld, "/")
+api.add_resource(Register, "/api/register")
+api.add_resource(Login, "/api/login")
